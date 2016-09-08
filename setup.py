3fcf3b7e48f9e2f78fc11782be306863bc1d38a3
Dmitry <dmitry.kalinin.email@gmail.com>
2016-09-08 11:21:13 +0300

    0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-api-instagram',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     author='Dmitry Kalinin',
     author_email='dmitry.kalinin.email@gmail.com',
